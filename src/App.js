7952d6c760b3f272c1bec810fb34c6c544f34584
Nicole <nicoletosello@georgebrown.com>
2021-11-30 16:48:43 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import React, { useEffect, useState } from "react";
+ 
+ function App() {
+   const [weatherData, setWeatherData] = useState();
+                                   
+  
+   const getWeatherData = async () => {
+      const res = await fetch(
+         "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=3df38a09d948643c333c37a9233e6a94").then((res) => res.json());
+                                
+     setWeatherData(res);
+   };
 
-function App() {
-  return (
+   function convertTemp()
+   {
+
+   }
+   function getDay()
+   {
+    var a = new Date();
+    var weekdays = new Array(7);
+    weekdays[0] = "Sunday";
+    weekdays[1] = "Monday";
+    weekdays[2] = "Tuesday";
+    weekdays[3] = "Wednesday";
+    weekdays[4] = "Thursday";
+    weekdays[5] = "Friday";
+    weekdays[6] = "Saturday";
+    return weekdays[a.getDay()];
+   }
+
+     
+   useEffect(() => {
+     getWeatherData();
+   }, []);
+   
+   console.log(weatherData);
+   
+   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={ weatherData && "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"} alt="weather icon"/>
+      <h3>{getDay()}</h3>
+      <h3>{(weatherData && weatherData.name)}</h3>
+      <h3> feels like {weatherData && (weatherData.main.feels_like - 273.15).toFixed(1) + "\u00B0C"}</h3>
+      <h3>{weatherData && JSON.stringify(weatherData, null, 2)}</h3>
       </header>
-    </div>
+     </div> 
+     
   );
-}
-
+ }
+ 
 export default App;
